95fa9c9aada0a5f2f30d0144554719d7da560adc
GorantlaSandeep <sandeepgorantla035@gmail.com>
2023-08-15 23:18:58 +0530

    This is my commit

---
+++ class.java
@@ -0,0 +1,11 @@
+public class main {
+    float a = 9.78f;
+    public static void main(String[] args) {
+        main myobj  = new main();
+        System.out.println("a:myobj");
+        
+    }
+
+
+    
+}
